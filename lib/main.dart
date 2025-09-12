753b235c117906fed3fd242a5c6d82fd396020ff
Dwiki Yulian <79854897+qjvxvL@users.noreply.github.com>
2025-09-12 12:13:31 +0700

    first commit for deployment

--- lib/main.dart
+++ lib/main.dart
@@ -96,6 +96,7 @@ class MyApp extends StatelessWidget {
             theme: AppTheme.lightTheme,
             debugShowCheckedModeBanner: false,
             home: const SplashScreen(),
+            // my splash screen
           ),
         );
       },
